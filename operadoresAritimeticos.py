7a037bb13844fdd070b89ead6dc1793e665e2ea4
MADISON ALLYSON DA SILVA NOBRE <202404225445@alunos.unifanor.edu.br>
2025-04-22 20:11:17 -0300

    novo assuno

---
+++ operadoresAritimeticos.py
@@ -0,0 +1,7 @@
+#criar duas variaveis que recebem, respectivamente dois numeros.
+#imprima na tela:
+#soma
+
+numero1 = 10
+numero2 = 15
+
---
+++ variaveis.py
@@ -0,0 +1,7 @@
+
+nome = "Madison"
+curso = "Engenharia Civil"
+anoCurso = "Terceiro Semestre"
+ira = 9
+print(nome, curso, anoCurso, ira)
+print ("Teu nome é {nome}, você cursa {curso}, e está no {anoCurso}, e sua pontuação é{ira}")
